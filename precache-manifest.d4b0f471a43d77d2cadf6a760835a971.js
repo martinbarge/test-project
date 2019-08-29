13b4ffbf21db4224c713e90d071d56ddcd988d7b
Martin Barge <m.i.barge@qmul.ac.uk>
2019-08-29 11:49:42 +0100

    Updates

---
+++ precache-manifest.d4b0f471a43d77d2cadf6a760835a971.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8058338af67b061b6eb014a5943181a",
+    "url": "/test-project/index.html"
+  },
+  {
+    "revision": "a1680e5fcd727752e6ff",
+    "url": "/test-project/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "530bc1705b6e8b877f11",
+    "url": "/test-project/static/js/2.e2d25cc1.chunk.js"
+  },
+  {
+    "revision": "a1680e5fcd727752e6ff",
+    "url": "/test-project/static/js/main.e8193190.chunk.js"
+  },
+  {
+    "revision": "6f33a5bf7c189c4d16dd",
+    "url": "/test-project/static/js/runtime~main.dc2dddcd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
